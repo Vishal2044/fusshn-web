cd0a679b379035e5eda407e98ee5873b8df44512
Vishal Varotariya <varotariyavishal2044@gmail.com>
2025-03-31 17:40:41 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration object containing project credentials
 const firebaseConfig = {
-  apiKey: "AIzaSyD_qheSZktpfGmF-txgA7saVD3oCVYpRHw",
-  authDomain: "mad-over-grills.firebaseapp.com",
-  projectId: "mad-over-grills",
-  storageBucket: "mad-over-grills.firebasestorage.app",
-  messagingSenderId: "938529738100",
-  appId: "1:938529738100:web:41dcb8e2663237b51b3021",
-  measurementId: "G-QZMY5SQDPN"
+  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase app
